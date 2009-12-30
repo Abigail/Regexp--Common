e1095aae81a9dd3e23393e231a89ec82d7a4163b
Abigail <abigail@abigail.be>
2009-12-30 03:24:05 +0100

    Another ack

--- lib/Regexp/Common.pm
+++ lib/Regexp/Common.pm
@@ -824,7 +824,8 @@ project, especially: Elijah, Jarkko, Tom, Nat, Ed, and Vivek.
 
 Further thanks go to: Alexandr Ciornii, Blair Zajac, Bob Stockdale,
 Charles Thomas, Chris Vertonghen, the CPAN Testers, Fany, Geoffrey Leach,
-Hermann-Marcus Behrens, Jim Cromie, Mike Arms, Mike Castle, Murat Uenalan,
+Hermann-Marcus Behrens, Jim Cromie, Mike Arms, Mike Castle, Mikko,
+Murat Uenalan,
 Rafael Garcia Suarez, Ron Savage, Sam Vilain, Slaven Rezic, Smylers,
 Tim Maher, and all the others I've forgotten.
 
